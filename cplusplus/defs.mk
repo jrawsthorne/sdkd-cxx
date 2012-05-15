59dbe5e12678976ec120e8911d0a6b4825ccb2fb
Mark Nunberg <mnunberg@haskalah.org>
2012-05-15 14:04:41 -0700

    Added platform-specific Makefiles

---
+++ cplusplus/defs.mk
@@ -0,0 +1,26 @@
+JSONCPP_LFLAGS := $(shell pkg-config jsoncpp --libs)
+JSONCPP_CPPFLAGS := $(shell pkg-config jsoncpp --cflags)
+
+LCB_LFLAGS := -lcouchbase
+LCB_CPPFLAGS :=
+
+CXX := g++
+CC := gcc
+
+OBJECTS := \
+	Message.o \
+	Request.o \
+	Dataset.o \
+	Response.o \
+	contrib/debug.o \
+	Handle.o \
+	IODispatch.o
+
+
+LDFLAGS = -lpthread
+LDFLAGS += $(JSONCPP_LFLAGS)
+LDFLAGS += $(LCB_LFLAGS)
+
+CPPFLAGS = -g -Wall
+CPPFLAGS += -Wno-reorder -pthread
+CPPFLAGS += $(JSONCPP_CPPFLAGS) $(LCB_CPPFLAGS)
