649aa7c138090a889620739df40b653d35d26225
Mark Nunberg <mnunberg@haskalah.org>
2012-05-30 18:38:43 -0700

    made rebalance op clearer (hopefully)

--- pytests/sdk/rebalance.py
+++ pytests/sdk/rebalance.py
@@ -36,7 +36,7 @@ class RebalanceBasic(SDKBaseTestCase):
         self.log.info("Created getter handle")
         
         # Create a dataset..
-        ds = DSSeed(1000,
+        ds = DSSeed(10000,
                     kseed = 'SimpleKey_',
                     vseed = 'SimpleValue_',
                     ksize = 12,
@@ -49,16 +49,15 @@ class RebalanceBasic(SDKBaseTestCase):
                                      wait = True)
         self.assertTrue(resp.is_ok())
         
+        # This seems to be the only incantation i can actually get working?
+        known = [ self.servers[0] ]
+        to_add = [ self.servers[1] ]
+        to_remove = []
         
-        task = None
-        for i in range(self.num_servers)[1:]:
-            task = self.cluster.async_rebalance(self.servers[:i],
-                                                     [self.servers[i]],
-                                                     [])
-            break
+        task = self.cluster.async_rebalance(known, to_add, to_remove)
         
-        setreq = setter.ds_mutate(ds, DelayMsec = 10)
-        getreq = getter.ds_retrieve(ds, DelayMsec = 10)
+        setreq = setter.ds_mutate(ds, DelayMax = 10, DelayMin = 1)
+        getreq = getter.ds_retrieve(ds, DelayMax = 10, DelayMin = 1)
         
         for h, r in ([setter, setreq], [getter,getreq]):
             h.invoke_command(r, wait = False)
@@ -78,3 +77,23 @@ class RebalanceBasic(SDKBaseTestCase):
         task.result()
         
         [ self.addHandle(h) for h in (getter, setter) ]
+    
+    def spamset(self):
+        """
+        This test will relentlessly spam the server with large keys and values
+        without any delays from 5 threads
+        """
+        ds = DSSeed(100000,
+                    kseed = 'LargeKey_',
+                    vseed = 'LargeValue_',
+                    repeat = 'HUGE',
+                    ksize = 256,
+                    vsize = 16384)
+        
+        spammers = []
+        for x in xrange(0, 4):
+            h = self.getDriver().create_handle(**self.getBucketParams())
+            h.invoke_command(h.ds_mutate(ds), wait = False)
+            
+        
+            
