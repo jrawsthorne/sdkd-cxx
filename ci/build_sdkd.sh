b1f59596528632757b583df59396a03c823ac699
Mark Nunberg <mnunberg@haskalah.org>
2012-08-07 15:51:38 -0700

    append to makefile variables rather than clobbering them

--- ci/build_sdkd.sh
+++ ci/build_sdkd.sh
@@ -15,9 +15,9 @@ cd $WORKSPACE/sdkd-cpp
 
 make -C src clean
 make -C src \
-    JSONCPP_CPPFLAGS="-I$JP/include" \
-    JSONCPP_LFLAGS="-L$JP/lib -Wl,-rpath=$JP/lib -ljson-cpp" \
-    LCB_CPPFLAGS="-I$CI_ROOT/include" \
+    JSONCPP_CPPFLAGS+="-I$JP/include" \
+    JSONCPP_LFLAGS+="-L$JP/lib -Wl,-rpath=$JP/lib -ljson-cpp" \
+    LCB_CPPFLAGS+="-I$CI_ROOT/include" \
     LCB_LFLAGS="-Wl,-rpath=$CI_ROOT/lib -L$CI_ROOT/lib -lcouchbase"
 
 cd $wd
