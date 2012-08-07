d2c59f3b0fccfa75e00f614b157a1fbd81ea7b1e
Mark Nunberg <mnunberg@haskalah.org>
2012-08-07 14:15:24 -0700

    Added perl bootstrap script for testing

---
+++ bootstrap.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+#
+package LCB_Dep;
+use Class::Struct;
+use strict;
+use warnings;
+
+struct __PACKAGE__, [
+    'release' => '$',
+    'url' => '$',
+    'urlbase' => '$',
+    'name' => '$'
+];
+
+
+
+use Getopt::Long;
+use Cwd qw(getcwd);
+
+GetOptions(
+    'p|prefix=s' => \my $Prefix,
+    'r|release=s' => \my $Release,
+    'b|vbrel=s' => \my $Vbucket
+);
+
+if ( (!$Prefix) || (!$Release) ) {
+    die ("Must have prefix and release");
+}
+
+my $p_prefix = "$Prefix/$Release";
+
+if (-e "$p_prefix/lib/libcouchbase.so" && -e "$p_prefix/lib/libvbucket.so") {
+    exit(0);
+}
+sub sys_or_die {
+    my @cmds = shift;
+    system(@cmds) == 0 or die "@cmds";
+}
+
+system("rm -rf $p_prefix");
+
+if (!$Vbucket) {
+    $Vbucket = "1.8.0.4";
+}
+my $CBURL = "http://packages.couchbase.com/clients/c";
+
+my @pkgs = (
+    LCB_Dep->new(
+        name => "libvbucket",
+        release => $Vbucket,
+        urlbase => $CBURL,
+    ),
+    LCB_Dep->new(
+        name => "libevent",
+        release => "2.0.19-stable",
+        urlbase => "https://github.com/downloads/libevent/libevent",
+    ),
+    LCB_Dep->new(
+        name => "libcouchbase",
+        release => $Release,
+        urlbase => $CBURL
+    )
+);
+
+my $old_dir = getcwd();
+
+sub append_to_env {
+    my ($var,$add) = @_;
+    $ENV{$var} = "$ENV{$var}:$add";
+}
+
+append_to_env("LD_LIBRARY_PATH", "$p_prefix/lib");
+append_to_env("C_INCLUDE_PATH", "$p_prefix/include");
+append_to_env("CPLUS_INCLUDE_PATH", "$p_prefix/include");
+append_to_env("LD_RUN_PATH", "$p_prefix/lib");
+
+foreach my $pkg (@pkgs) {
+    chdir $old_dir;
+    my $dir = sprintf("%s-%s",  $pkg->name, $pkg->release);
+    my $tarball = "$dir.tar.gz";
+
+    if (!-e $tarball) {
+        my $url = sprintf("%s/$tarball", $pkg->urlbase);
+        sys_or_die("wget $url");
+    }
+
+    sys_or_die("rm -rf $dir");
+    sys_or_die("tar xf $tarball");
+
+    chdir($dir) or die "chdir: $!";
+    sys_or_die("./configure --prefix=$p_prefix");
+    sys_or_die("make");
+    sys_or_die("make install");
+}
