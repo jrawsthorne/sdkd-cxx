5243cfe7f5303cbb69017dd78caebab1354e2002
Mark Nunberg <mnunberg@haskalah.org>
2012-08-07 15:46:47 -0700

    env script..

--- ci/bootstrap.pl
+++ ci/bootstrap.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl
-#
+
 package LCB_Dep;
-use Class::Struct;
 use strict;
 use warnings;
+
+use Getopt::Long;
+use Cwd qw(getcwd);
+use Class::Struct;
 
 struct __PACKAGE__, [
     'release' => '$',
@@ -13,10 +16,6 @@ struct __PACKAGE__, [
     'configure_flags' => '$',
 ];
 
-
-
-use Getopt::Long;
-use Cwd qw(getcwd);
 
 GetOptions(
     'p|prefix=s' => \my $Prefix,
@@ -30,20 +29,35 @@ if ( (!$Prefix) || (!$Release) ) {
 
 my $p_prefix = "$Prefix/$Release";
 
-if (-e "$p_prefix/lib/libcouchbase.so" && -e "$p_prefix/lib/libvbucket.so") {
-    exit(0);
-}
+my $need_deps = 
+    !((-e "$p_prefix/lib/libcouchbase.so" && 
+        -e "$p_prefix/lib/libvbucket.so"));
+
 sub sys_or_die {
     my @cmds = shift;
     system(@cmds) == 0 or die "@cmds";
 }
 
-system("rm -rf $p_prefix");
 
 if (!$Vbucket) {
     $Vbucket = "1.8.0.4";
 }
 my $CBURL = "http://packages.couchbase.com/clients/c";
+
+
+my $old_dir = getcwd();
+
+sub append_to_env {
+    my ($var,$add) = @_;
+    $ENV{$var} = "$ENV{$var}:$add";
+}
+
+append_to_env("LD_LIBRARY_PATH", "$p_prefix/lib");
+append_to_env("C_INCLUDE_PATH", "$p_prefix/include");
+append_to_env("CPLUS_INCLUDE_PATH", "$p_prefix/include");
+append_to_env("LD_RUN_PATH", "$p_prefix/lib");
+$ENV{LDFLAGS} = "-L$p_prefix/lib";
+$ENV{CPPFLAGS} = "-I$p_prefix/include";
 
 my @pkgs = (
     LCB_Dep->new(
@@ -61,41 +75,40 @@ my @pkgs = (
         release => $Release,
         urlbase => $CBURL,
         configure_flags => 
-            ' --disable-tools --disable-couchbasemock'
+        ' --disable-tools --disable-couchbasemock'
     )
 );
 
-my $old_dir = getcwd();
+sub build_deps {
+    system("rm -rf $p_prefix");
 
-sub append_to_env {
-    my ($var,$add) = @_;
-    $ENV{$var} = "$ENV{$var}:$add";
-}
+    foreach my $pkg (@pkgs) {
+        chdir $old_dir;
+        my $dir = sprintf("%s-%s",  $pkg->name, $pkg->release);
+        my $tarball = "$dir.tar.gz";
 
-append_to_env("LD_LIBRARY_PATH", "$p_prefix/lib");
-append_to_env("C_INCLUDE_PATH", "$p_prefix/include");
-append_to_env("CPLUS_INCLUDE_PATH", "$p_prefix/include");
-append_to_env("LD_RUN_PATH", "$p_prefix/lib");
+        if (!-e $tarball) {
+            my $url = sprintf("%s/$tarball", $pkg->urlbase);
+            sys_or_die("wget $url");
+        }
 
-$ENV{LDFLAGS} = "-L$p_prefix/lib";
-$ENV{CPPFLAGS} = "-I$p_prefix/include";
+        sys_or_die("rm -rf $dir");
+        sys_or_die("tar xf $tarball");
 
-foreach my $pkg (@pkgs) {
-    chdir $old_dir;
-    my $dir = sprintf("%s-%s",  $pkg->name, $pkg->release);
-    my $tarball = "$dir.tar.gz";
-
-    if (!-e $tarball) {
-        my $url = sprintf("%s/$tarball", $pkg->urlbase);
-        sys_or_die("wget $url");
+        chdir($dir) or die "chdir: $!";
+        sys_or_die("./configure --prefix=$p_prefix " .
+            $pkg->configure_flags);
+        sys_or_die("make");
+        sys_or_die("make install");
     }
-
-    sys_or_die("rm -rf $dir");
-    sys_or_die("tar xf $tarball");
-
-    chdir($dir) or die "chdir: $!";
-    sys_or_die("./configure --prefix=$p_prefix " .
-        $pkg->configure_flags);
-    sys_or_die("make");
-    sys_or_die("make install");
 }
+
+if ($need_deps) {
+    build_deps();
+}
+
+my $fname = "sdkd-cpp-env.sh";
+open my $fh, ">", $fname or die "$fname: $!";
+print $fh <<"EOF";
+export CI_ROOT=$p_prefix/lib
+EOF
