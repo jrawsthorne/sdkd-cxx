83b8cc162469984334e03979bbbefb989b5d541e
Mark Nunberg <mnunberg@haskalah.org>
2012-08-07 15:10:14 -0700

    Made CI directory, added env source script and build script

---
+++ ci/build_sdkd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. $WORKSPACE/sdkd-cpp-env.sh
+JP=$WORKSPACE/json-cpp
+wd=$PWD
+
+cd $JP
+make -f makefiles/gcc/Makefile
+
+cd $wd
+cd $WORKSPACE/sdkd-cpp
+
+make -C src clean
+make -C src \
+    JSONCPP_CPPFLAGS=-I$JP/include \
+    JSONCPP_LFLAGS=-L$JP/lib -Wl,-rpath=$JP/lib -ljson-cpp \
+    LCB_CPPFLAGS=-I$CI_ROOT/include \
+    LCB_LFLAGS=-Wl,-rpath=$CI_ROOT/lib -L$CI_ROOT/lib -lcouchbase
+
+cd $wd
