72ce28ccd75f4207ac8c3e5b504f3017c04f6480
Subhashni Balakrishnan <b.subhashni@gmail.com>
2015-07-06 11:53:31 -0700

    Add time to logs

--- src/logging.h
+++ src/logging.h
@@ -19,7 +19,7 @@ extern "C" {
 
 class Logger : public lcb_logprocs_st {
     public:
-        Logger(int level, const char *filename) : file(NULL)
+        Logger(int level, const char *filename) :start_time(0), file(NULL)
         {
             minlevel = level;
             fp = fopen(filename, "a");
@@ -37,6 +37,18 @@ class Logger : public lcb_logprocs_st {
                 fclose(fp);
             }
         }
+
+        uint64_t gethrtime() {
+            uint64_t ret = 0;
+            struct timeval tv;
+            if (gettimeofday(&tv, NULL) == -1) {
+                return -1;
+            }
+            ret = (uint64_t)tv.tv_sec * 1000000000;
+            ret += (uint64_t)tv.tv_usec * 1000;
+            return ret;
+        }
+
         void log(unsigned int iid,
                 const char *subsys,
                 int severity,
@@ -45,8 +57,16 @@ class Logger : public lcb_logprocs_st {
                 const char *fmt,
                 va_list ap)
         {
+            uint64_t now = gethrtime();
+            if (!start_time) {
+                start_time = gethrtime();
+            }
+            if (now == start_time) {
+                now++;
+            }
             flockfile(fp);
-            fprintf(fp, "[I%d] (%s - L:%d) ",
+            fprintf(fp, "%lums  [I%d] (%s - L:%d) ",
+                    (unsigned long)(now - start_time) /1000000,
                     iid,
                     subsys,
                     srcline);
@@ -55,6 +75,7 @@ class Logger : public lcb_logprocs_st {
             funlockfile(fp);
         }
     private:
+        uint64_t start_time;
         const char *file;
         FILE *fp;
         int minlevel;
