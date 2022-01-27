dea4aa185d1c890e6a145f23b60435f115a98ad7
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2022-01-27 13:02:29 +0000

    Append current time to the log file name so tests have a unique log file

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,5 +1,6 @@
 #include "sdkd_internal.h"
 #include <couchbase/logger/configuration.hxx>
+#include <ctime>
 
 namespace CBSdkd
 {
@@ -10,8 +11,13 @@ create_logger(const std::string& path)
     // sdkd expects a static filename whereas the defaul client logger uses rotating logs
     // Use a console logger for now
     if (!couchbase::logger::is_initialized()) {
+
+        // append current time to the log file name so tests have a unique log file
+        auto now = std::chrono::steady_clock::now();
+        auto real_path = fmt::format("{}.{}", path, now.time_since_epoch().count());
+
         // auto logger = spdlog::basic_logger_mt(LOGGER_NAME, path);
-        couchbase::logger::configuration configuration{ path };
+        couchbase::logger::configuration configuration{ real_path };
         configuration.log_level = couchbase::logger::level::trace;
         couchbase::logger::create_file_logger(configuration);
         // couchbase::logger::create_console_logger();
